d81f50c173f36ca9a2a9a9e12614f9482db4f93c
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-03-28 17:13:17 -0400

    Built out create, destroy funtionality and the form for collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,5 @@
 class CollectionsController < ApplicationController
+  before_action :set_collection, only: [:show, :edit, :update, :destroy]
   layout 'portfolio'
   def index
     @collections = Collection.all
@@ -6,7 +7,39 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = Collection.find(params[:id])
-    @portfolio_items = @collection.portfolios.by_position
+    @portfolio_items = @collection.portfolios
   end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    respond_to do |format|
+      if @collection.save
+        format.html { redirect_to @collection, notice: 'Collection was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @collection.destroy
+    respond_to do |format|
+      format.html { redirect_to collections_url, notice: 'Collection was successfully removed.' }
+    end
+  end
+
+
+  private
+    def set_collection
+      @collection = Collection.find(params[:id])
+    end
+
+    def collection_params
+      params.require(:collection).permit(:title)
+    end
 
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,4 @@
 class Collection < ApplicationRecord
   validates_presence_of :title
-  has_many :portfolios
+  has_many :portfolios, dependent: :destroy
 end
