10cd75c230d349abffee7313aed39582020177bc
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-03-30 15:23:19 -0400

    also updated schema and forgot to commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328155005) do
+ActiveRecord::Schema.define(version: 20180330190612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180328155005) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "main_image"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
